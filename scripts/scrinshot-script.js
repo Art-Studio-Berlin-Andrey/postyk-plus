cfaeb0767d37a29b33c41a03d66dcedce9a20247
Art-Studio-Berlin-Andrey <116876928+Art-Studio-Berlin-Andrey@users.noreply.github.com>
2023-02-14 13:32:10 +0200

    Update scrinshot-script.js

--- scripts/scrinshot-script.js
+++ scripts/scrinshot-script.js
@@ -1,7 +1,7 @@
 function capture () {
   html2canvas(document.getElementById("out")).then((canvas) => {
     let a = document.createElement("a");
-    a.download = "by-postyk.png";
+    a.download = "postyk.png";
     a.href = canvas.toDataURL("image/png");
     a.click(); // MAY NOT ALWAYS WORK!
   });
